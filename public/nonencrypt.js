4dd07db2879c05844d0f44d6f7b3a8978ab4dba8
ghifari-ti <muhammad.ghifari@students.paramadina.ac.id>
2021-11-14 23:15:04 +0700

    fix os 2

--- public/nonencrypt.js
+++ public/nonencrypt.js
@@ -55,7 +55,7 @@ $(() => {
               element["last_seen"]
             )}</div>
             <div><span class="font-medium">OS</span>: ${
-              element["state"].hasOwnProperty("os") ? element["state"]["os"] : ""
+              ("os" in element["state"]) ? element["state"]["os"] : ""
             }</div>
             <div><span class="font-medium">State</span>: ${
               element["state"]["windowName"]
